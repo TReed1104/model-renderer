33fa2347519389bf363d7a1fdea3bffb47bc8535
Tom Reed <t.reed1104@gmail.com>
2020-09-29 00:54:18 +0100

    Retrieved webgl context from canvas area

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function main() {
+    // Prep the canvas and get our webgl context
+    let canvas = document.getElementById('main-canvas')
+    let webgl = canvas.getContext('experimental-webgl');
 }
 
 main();
