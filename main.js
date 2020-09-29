962c94d14dc80e72122bbb45db25d5ddbee27fa0
Tom Reed <t.reed1104@gmail.com>
2020-09-29 01:03:45 +0100

    Basic shader source code

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 function main() {
+    // Shader Source Code
+    var vertexShaderCode = '\
+        attribute vec2 coordinates; \
+        void main(void) { \
+            gl_Position = vec4(coordinates,0.0, 1.0); \
+        }';
+    var fragmentShaderCode = '\
+        void main(void) { \
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1); \
+        }';
+
     // Prep the canvas and get our webgl context
     let canvas = document.getElementById('main-canvas')
     let webgl = canvas.getContext('experimental-webgl');
