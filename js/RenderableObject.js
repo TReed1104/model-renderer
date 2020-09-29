856b884e0ee9aa83a35106a6486c3a3d734b1071
Tom Reed <t.reed1104@gmail.com>
2020-09-29 15:37:33 +0100

    Commenting in RO class file

--- js/RenderableObject.js
+++ js/RenderableObject.js
@@ -6,10 +6,13 @@ class RenderableObject {
 
         // Index in the shader register of the shader for this object
         this.shaderIndex = 0;
+
+        // Vertex data for rendering the object
         this.vertices = vertices;
         this.indices = indices;
         this.colours = colour;
 
+        // Create the Vertex array object for the object
         this.vao = webgl.createVertexArray();
         webgl.bindVertexArray(this.vao);
 
@@ -19,6 +22,7 @@ class RenderableObject {
         webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array(this.vertices), webgl.STATIC_DRAW);
         webgl.vertexAttribPointer(0, 3, webgl.FLOAT, false, 0, 0);
         webgl.enableVertexAttribArray(0);
+        webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
 
         // Create a new buffer for the indices
         this.indicesBuffer = webgl.createBuffer();
@@ -31,6 +35,7 @@ class RenderableObject {
         webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array(this.colours), webgl.STATIC_DRAW);
         webgl.vertexAttribPointer(1, 3, webgl.FLOAT, false, 0, 0);
         webgl.enableVertexAttribArray(1);
+        webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
 
         webgl.bindVertexArray(null);
     }
@@ -39,10 +44,13 @@ class RenderableObject {
     }
 
     draw(shader) {
+        // Enable the shader for the object
         shader.enable()
         webgl.bindVertexArray(this.vao);
+        // Render the object
         webgl.drawElements(webgl.TRIANGLES, this.indices.length, webgl.UNSIGNED_SHORT, 0);
         webgl.bindVertexArray(null);
+        // Disable the shader for the object - Make sure its cleared for the next object
         shader.disable()
     }
 }
