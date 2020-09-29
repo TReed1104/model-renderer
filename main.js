97d694947b20e31384495026f3447bcbf810839b
Tom Reed <t.reed1104@gmail.com>
2020-09-29 13:32:21 +0100

    Migrated main to use the RenderableObject

--- main.js
+++ main.js
@@ -80,60 +80,23 @@ function compileShaderProgram(vertexSource, fragmentSource) {
 
 // Main function
 function main() {
-    // Vertices
-    let vertices = [
-        -0.5, 0.5, 0.0,
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.5, 0.5, 0.0
-    ];
-    // Indices
-    let indices = [3, 2, 1, 3, 1, 0];
-    // Colours
-    let colours = [0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1,];
-
-    // Create a new buffer for the vertices
-    let vertexBuffer = webgl.createBuffer();
-    webgl.bindBuffer(webgl.ARRAY_BUFFER, vertexBuffer);
-    webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array(vertices), webgl.STATIC_DRAW);
-    webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
-    // Create a new buffer for the indices
-    let indicesBuffer = webgl.createBuffer();
-    webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
-    webgl.bufferData(webgl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), webgl.STATIC_DRAW);
-    webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, null);
-    // Create a new buffer for the colour data
-    let colourBuffer = webgl.createBuffer();
-    webgl.bindBuffer(webgl.ARRAY_BUFFER, colourBuffer);
-    webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array(colours), webgl.STATIC_DRAW);
-    webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
-
     let basicShaderProgram = compileShaderProgram(vertexShaderCode, fragmentShaderCode);
+    
+    // Create a basic GameObject
+    let vertices = [ -0.5, 0.5, 0.0, -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.5, 0.5, 0.0 ];
+    let indices = [ 3, 2, 1, 3, 1, 0, ];
+    let colours = [ 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, ];
+    let basicObject = new RenderableObject(basicShaderProgram,vertices, indices, colours);
 
     // Start a Render
     webgl.viewport(0, 0, canvas.width, canvas.height);
     webgl.clearColor(0.5, 0.5, 0.5, 0.9);
     webgl.clear(webgl.COLOR_BUFFER_BIT | webgl.DEPTH_BUFFER_BIT);
-    webgl.useProgram(basicShaderProgram);
-        webgl.enable(webgl.DEPTH_TEST);
-            webgl.bindBuffer(webgl.ARRAY_BUFFER, vertexBuffer);
-                webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
-                    // Get the attribute location for the coordinates
-                    let coordShaderLocation = webgl.getAttribLocation(basicShaderProgram, "vertexPosition");
-                    webgl.vertexAttribPointer(coordShaderLocation, 3, webgl.FLOAT, false, 0, 0);
-                    webgl.enableVertexAttribArray(coordShaderLocation);
-                    webgl.bindBuffer(webgl.ARRAY_BUFFER, colourBuffer);
-                        // Get the 
-                        let colourShaderLocation = webgl.getAttribLocation(basicShaderProgram, "vertexColour");
-                        webgl.vertexAttribPointer(colourShaderLocation, 3, webgl.FLOAT, false,0,0) ;
-                        webgl.enableVertexAttribArray(colourShaderLocation);
-                        // Draw the vertices
-                        webgl.drawElements(webgl.TRIANGLES, indices.length, webgl.UNSIGNED_SHORT, 0);
-                    webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
-                webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, null);
-            webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
-        webgl.disable(webgl.DEPTH_TEST);
-    webgl.useProgram(null);
+    webgl.enable(webgl.DEPTH_TEST);
+
+    basicObject.draw(basicShaderProgram);
+
+    webgl.disable(webgl.DEPTH_TEST);
 }
 
 main();
