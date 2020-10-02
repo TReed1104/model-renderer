de9e4c67cb8b121aefb2bd83aaaacf4b1e432aab
Tom Reed <t.reed1104@gmail.com>
2020-10-02 12:09:31 +0100

    Rewrote the vertex and fragment shader source code

--- js/Engine.js
+++ js/Engine.js
@@ -56,25 +56,30 @@ class Engine {
     // Load the shaders
     loadShaders() {
         // Basic Vertex Shader Source Code
-        let vertexShaderSourceCode = '\
-        attribute vec3 vertexPosition; \
-        attribute vec3 vertexColour; \
-        varying vec3 fragmentColour; \
-        uniform mat4 pMatrix; \
-        uniform mat4 vMatrix; \
-        uniform mat4 mMatrix; \
-        void main(void) { \
-            gl_Position = pMatrix * vMatrix * mMatrix * vec4(vertexPosition, 1.0); \
-            fragmentColour = vertexColour; \
-        }';
+        let vertexShaderSourceCode = `#version 300 es
+            layout (location=0) in vec3 vertexPosition;
+            layout (location=1) in vec3 vertexColour;
+            out vec3 fragmentColour;
+            uniform mat4 pMatrix;
+            uniform mat4 vMatrix;
+            uniform mat4 mMatrix;
+
+            void main() {
+                fragmentColour = vertexColour;
+                gl_Position = pMatrix * vMatrix * mMatrix * vec4(vertexPosition, 1.0);
+            }
+        `;
 
         // Basic Fragment Shader Source Code
-        let fragmentShaderSourceCode = '\
-        precision mediump float; \
-        varying vec3 fragmentColour; \
-        void main(void) { \
-            gl_FragColor = vec4(fragmentColour, 1.); \
-        }';
+        let fragmentShaderSourceCode = `#version 300 es
+            precision highp float;
+            in vec3 fragmentColour;
+            out vec4 outputColour;
+
+            void main(void) {
+                outputColour = vec4(fragmentColour, 1.0);
+            }
+        `;
 
         // Create the shader
         this.shaderRegister.push(new Shader(vertexShaderSourceCode, fragmentShaderSourceCode));
