d50683335d83289716d358929ecd95b63fdc1b12
Tom Reed <t.reed1104@gmail.com>
2020-10-04 00:25:02 +0100

    Amended the textured shader to have the texture sampler and UV data needed

--- shaders/Textured.js
+++ shaders/Textured.js
@@ -3,10 +3,12 @@ var VertexCode =
     // Vertex In - From Program
     layout (location=0) in vec3 vertexPosition;
     layout (location=1) in vec3 vertexColour;
+    layout (location=2) in vec2 vertexUV;
 
     // Vertex Out - To Fragment
     out vec3 fragmentPosition;
     out vec3 fragmentColour;
+    out vec2 fragmentUV;
 
     // Uniforms - From Program
     uniform mat4 modelMatrix;
@@ -17,6 +19,7 @@ var VertexCode =
     void main() {
         fragmentPosition = vec3(modelMatrix * vec4(vertexPosition, 1.0));
         fragmentColour = vertexColour;
+        fragmentUV = vertexUV;
         gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vertexPosition, 1.0);
     }
 `;
@@ -28,13 +31,17 @@ var FragmentCode =
     // Fragment In - From Vertex
     in vec3 fragmentPosition;
     in vec3 fragmentColour;
+    in vec2 fragmentUV;
 
     // Fragment Out - To Frame Buffer
     out vec4 outputColour;
 
+    // Uniforms - From Program
+    uniform sampler2D textureSampler;
+
     // Fragment Shader Entry
     void main(void) {
-        outputColour = vec4(fragmentColour, 1.0f);
+        outputColour = texture2D(textureSampler, fragmentUV);
     }
 `;
 
