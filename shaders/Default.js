88de8128e5252bbbc35e0eb0d73b10672a550002
Tom Reed <t.reed1104@gmail.com>
2020-10-03 02:28:07 +0100

    Rewrite of the default shader - more in line with my default from ArkEngine

--- shaders/Default.js
+++ shaders/Default.js
@@ -1,21 +1,23 @@
 var VertexCode =
 `   #version 300 es
-    // Vertex Ins - From Program
+    // Vertex In - From Program
     layout (location=0) in vec3 vertexPosition;
     layout (location=1) in vec3 vertexColour;
 
-    // Vertex Outs - To Fragment
+    // Vertex Out - To Fragment
+    out vec3 fragmentPosition;
     out vec3 fragmentColour;
 
     // Uniforms - From Program
-    uniform mat4 pMatrix;
-    uniform mat4 vMatrix;
-    uniform mat4 mMatrix;
+    uniform mat4 modelMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 projectionMatrix;
     
     // Vertex Shader Entry
     void main() {
+        fragmentPosition = vec3(modelMatrix * vec4(vertexPosition, 1.0));
         fragmentColour = vertexColour;
-        gl_Position = pMatrix * vMatrix * mMatrix * vec4(vertexPosition, 1.0);
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(vertexPosition, 1.0);
     }
 `;
 
@@ -23,15 +25,16 @@ var FragmentCode =
 `   #version 300 es
     precision highp float;
 
-    // Fragment Ins - From Vertex
+    // Fragment In - From Vertex
+    in vec3 fragmentPosition;
     in vec3 fragmentColour;
 
-    // Fragment Outs - To Frame Buffer
+    // Fragment Out - To Frame Buffer
     out vec4 outputColour;
 
     // Fragment Shader Entry
     void main(void) {
-        outputColour = vec4(fragmentColour, 1.0);
+        outputColour = vec4(fragmentColour, 1.0f);
     }
 `;
 
