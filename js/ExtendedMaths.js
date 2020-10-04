5e8386b81f630b7d058b1c29bdda641bcfad9484
Tom Reed <t.reed1104@gmail.com>
2020-10-04 02:26:40 +0100

    Added a js module of helpful maths functions

---
+++ js/ExtendedMaths.js
@@ -0,0 +1,9 @@
+var ExtendedMaths = {
+    radiansToDegrees: function (r) {
+        return r * 180 / Math.PI;
+    },
+    degreesToRadians: function (d) {
+        return d * Math.PI / 180;
+    }
+}
+export default ExtendedMaths
