13f91fe7208a99342f8874f52544efa8e15fe172
Tom Reed <t.reed1104@gmail.com>
2020-10-05 00:03:11 +0100

    Texture class commenting

--- js/Texture.js
+++ js/Texture.js
@@ -15,23 +15,27 @@ export default class Texture {
     LoadTexture(texturePath) {
         var texture = webgl.createTexture();
         webgl.bindTexture(webgl.TEXTURE_2D, texture);
+        // Create a blank texture and fill it as a magenta texture
         webgl.texImage2D(webgl.TEXTURE_2D, 0, webgl.RGBA, 1, 1, 0, webgl.RGBA, webgl.UNSIGNED_BYTE, new Uint8Array([255, 0, 255, 255]));
+        // Load the texture file - this is asynchronous so we use event listeners to handle it
         var image = new Image();
         image.src = texturePath;
         // Load event for the textures
         image.addEventListener('load', () => {
-            // Now that the image has loaded make copy it to the texture.
+            // Bind to the image data to the webgl texture data
             webgl.bindTexture(webgl.TEXTURE_2D, texture);
             webgl.texImage2D(webgl.TEXTURE_2D, 0, webgl.RGBA, webgl.RGBA, webgl.UNSIGNED_BYTE, image);
             webgl.generateMipmap(webgl.TEXTURE_2D);
             // Set the sizing of the texture
             this.width = image.width;
             this.height = image.height;
+            // Set the load states - Sucess
             this.isLoaded = true;
             this.webglTexture = texture;
         });
         // Catch when a texture fails to load
         image.addEventListener('error',  () => {
+            // Set the load states - Failed
             this.isLoaded = false;
             this.webglTexture = null;
         });
