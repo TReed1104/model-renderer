c8b569c2b088ab4867592c1a4c29b8ac14ee5dca
Tom Reed <t.reed1104@gmail.com>
2020-10-04 02:29:13 +0100

    Amended the matrix4 moduel to match the improved version at https://webglfundamentals.org/webgl/lessons/webgl-3d-camera.html

--- js/Matrix4.js
+++ js/Matrix4.js
@@ -1,11 +1,53 @@
 export var matrix4 = {
-    projection: function(angle, aspect, zMin, zMax) {
-        var ang = Math.tan((angle * 0.5) * Math.PI / 180); //angle*0.5
+    subtractVectors: function (a, b) {
+        return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+    },
+    normalise: function (v) {
+        var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+        // make sure we don't divide by 0.
+        if (length > 0.00001) {
+            return [v[0] / length, v[1] / length, v[2] / length];
+        } else {
+            return [0, 0, 0];
+        }
+    },
+    cross: function (a, b) {
+        return [a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]];
+    },
+    lookAt: function(cameraPosition, target, up) {
+        var zAxis = matrix4.normalise(matrix4.subtractVectors(cameraPosition, target));
+        var xAxis = matrix4.normalise(matrix4.cross(up, zAxis));
+        var yAxis = matrix4.normalise(matrix4.cross(zAxis, xAxis));
+    
         return [
-            0.5 / ang, 0, 0, 0,
-            0, 0.5 * aspect / ang, 0, 0,
-            0, 0, -(zMax + zMin) / (zMax - zMin), -1,
-            0, 0, (-2 * zMax * zMin) / (zMax - zMin), 0
+           xAxis[0], xAxis[1], xAxis[2], 0,
+           yAxis[0], yAxis[1], yAxis[2], 0,
+           zAxis[0], zAxis[1], zAxis[2], 0,
+           cameraPosition[0],
+           cameraPosition[1],
+           cameraPosition[2],
+           1,
+        ];
+      },
+    perspective: function (angle, aspect, zMin, zMax) {
+        var f = Math.tan(Math.PI * 0.5 - 0.5 * angle);
+        var rangeInv = 1.0 / (zMin - zMax);
+        return [
+            f / aspect, 0, 0, 0,
+            0, f, 0, 0,
+            0, 0, (zMin + zMax) * rangeInv, -1,
+            0, 0, zMin * zMax * rangeInv * 2, 0
+        ];
+    },
+    projection: function (width, height, depth) {
+        // Note: This matrix flips the Y axis so 0 is at the top.
+        return [
+            2 / width, 0, 0, 0,
+            0, -2 / height, 0, 0,
+            0, 0, 2 / depth, 0,
+            -1, 1, 0, 1,
         ];
     },
     multiply: function (a, b) {
