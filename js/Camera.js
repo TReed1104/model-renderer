4b5e8a42ae6f2a6fff5fd0b409302a510ffbed51
Tom Reed <t.reed1104@gmail.com>
2020-10-06 00:25:20 +0100

    Reworked the Camera constructor

--- js/Camera.js
+++ js/Camera.js
@@ -3,9 +3,19 @@ import matrix4  from "./Matrix4.js"
 import ExtendedMaths from "./ExtendedMaths.js"
 
 export default class Camera {
-    constructor(id, position, lookAt, up, fovAngle, near, far) {
+    constructor(id, position, lookAt, up, fieldOfViewAngle, nearZ, farZ) {
+        // Set the object variables
         this.id = id;
-        this.projectionMatrix = matrix4.perspective(ExtendedMaths.degreesToRadians(fovAngle), canvas.width / canvas.height, near, far);
-        this.viewMatrix = matrix4.inverse(matrix4.lookAt(position, lookAt, up));
+        this.position = position;
+        this.lookAt = lookAt;
+        this.up = up;
+        this.fieldOfViewAngle = fieldOfViewAngle;
+        this.nearZ = nearZ;
+        this.farZ = farZ;
+
+        // Setup the camera matrix
+        this.projectionMatrix = matrix4.perspective(ExtendedMaths.degreesToRadians(this.fieldOfViewAngle), canvas.width / canvas.height, this.nearZ, this.farZ);
+        this.viewMatrix = matrix4.inverse(matrix4.lookAt(this.position, this.lookAt, this.up));
+    }
     }
 }
