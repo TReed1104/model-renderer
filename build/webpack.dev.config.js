6994f6bfdc73774b1f0c53cd886aeab9c03d596f
Tom Reed <t.reed1104@gmail.com>
2020-10-14 16:06:39 +0100

    Added webpack build configs

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,59 @@
+'use strict'
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+    mode: 'development',
+    entry: ['@babel/polyfill', './js/main.js'],
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        "presets": [[
+                            "@babel/preset-env", {
+                                "useBuiltIns": "entry"
+                            }]
+                        ],
+                        "plugins": [
+                            [
+                                "@babel/plugin-proposal-class-properties",
+                                {
+                                    "loose": true
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
+        new CopyWebpackPlugin([{
+            from: resolve('content'),
+            to: resolve('dist/content'),
+            toType: 'dir'
+        }])
+    ]
+}
