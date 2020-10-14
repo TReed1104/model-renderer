2ac780c70fd3622075ebcf7ca1edf36946d83527
Tom Reed <t.reed1104@gmail.com>
2020-10-14 08:43:08 +0100

    Model constructor

--- js/Model.js
+++ js/Model.js
@@ -2,5 +2,23 @@ import matrix4 from "./Matrix4.js"
 import Mesh from "./Mesh.js"
 
 export default class Model {
+    constructor(modelFileLocation, position, rotation, scale) {
+        this.id = id;
+        this.modelFileLocation = modelFileLocation;
+        this.meshes = [];
+
+        this.loadModel(this.modelFileLocation);
+
+        // Setup the transformation data
+        this.position = position
+        this.rotation = rotation
+        this.scale = scale
+
+        // Setup the model matrix
+        this.baseMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+        this.translationMatrix = matrix4.translate(this.baseMatrix, this.position);
+        this.rotationMatrix = matrix4.rotate(this.baseMatrix, this.rotation);
+        this.scaleMatrix = matrix4.scale(this.baseMatrix, this.scale);
+        this.modelMatrix = matrix4.multiply(matrix4.multiply(this.translationMatrix, this.rotationMatrix), this.scaleMatrix);
     }
 }
