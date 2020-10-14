5d71430bc61a46909acf9227b4a44d3205b90851
Tom Reed <t.reed1104@gmail.com>
2020-10-14 08:43:34 +0100

    Loaded model function signature

--- js/Model.js
+++ js/Model.js
@@ -21,6 +21,11 @@ export default class Model {
         this.scaleMatrix = matrix4.scale(this.baseMatrix, this.scale);
         this.modelMatrix = matrix4.multiply(matrix4.multiply(this.translationMatrix, this.rotationMatrix), this.scaleMatrix);
     }
+
+    loadModel(modelFileLocation) {
+
+    }
+
     update(deltaTime) {
         this.translationMatrix = matrix4.translate(this.baseMatrix, this.position);
         this.rotationMatrix = matrix4.rotate(this.baseMatrix, this.rotation);
