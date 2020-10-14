160e98b3991b6162da9a3a4db3e01725b7eaed21
Tom Reed <t.reed1104@gmail.com>
2020-10-14 08:41:12 +0100

    Mesh constructor

--- js/Mesh.js
+++ js/Mesh.js
@@ -3,5 +3,6 @@ import matrix4 from "./Matrix4.js"
 import Texture from "./Texture.js"
 
 export default class Mesh {
+    constructor(indices, colour, uvs, normals, position, rotation, scale, textureFileLocation) {
     }
 }
