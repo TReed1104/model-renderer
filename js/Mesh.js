c9ac1bf62ce80e98d7c67f7f61096da85da0680b
Tom Reed <t.reed1104@gmail.com>
2020-10-14 08:41:57 +0100

    Copied over setup from the RenderableObject class

--- js/Mesh.js
+++ js/Mesh.js
@@ -4,5 +4,30 @@ import Texture from "./Texture.js"
 
 export default class Mesh {
     constructor(indices, colour, uvs, normals, position, rotation, scale, textureFileLocation) {
+        // Cache the object data
+        this.vertices = vertices;
+        this.indices = indices;
+        this.colours = colour;
+        this.uvs = uvs;
+        this.normals = normals;
+
+        // Texturing
+        this.isObjectUVMapped = false;
+        this.texture = new Texture(textureFileLocation, textureFileLocation);
+
+        // Generate the Vertex array object
+        this.vertexArray = this.generateVertexArray(this.vertices, this.indices, this.colours, this.uvs, this.normals);
+
+        // Setup the transformation data
+        this.position = position
+        this.rotation = rotation
+        this.scale = scale
+
+        // Setup the model matrix
+        this.baseMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+        this.translationMatrix = matrix4.translate(this.baseMatrix, this.position);
+        this.rotationMatrix = matrix4.rotate(this.baseMatrix, this.rotation);
+        this.scaleMatrix = matrix4.scale(this.baseMatrix, this.scale);
+        this.modelMatrix = matrix4.multiply(matrix4.multiply(this.translationMatrix, this.rotationMatrix), this.scaleMatrix);
     }
 }
