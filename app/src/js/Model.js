0da5f6599734731eb77c57e8f6f7c8af3d068f66
Tom Reed <t.reed1104@gmail.com>
2020-10-18 00:31:42 +0100

    Replaced the raw matrix4 creation with matrix4.creatE()

--- app/src/js/Model.js
+++ app/src/js/Model.js
@@ -23,7 +23,7 @@ export default class Model {
         this.scale = scale
 
         // Setup the model matrix
-        this.baseMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+        this.baseMatrix = matrix4.create(1);
         this.translationMatrix = matrix4.translate(this.baseMatrix, this.position);
         this.rotationMatrix = matrix4.rotate(this.baseMatrix, this.rotation);
         this.scaleMatrix = matrix4.scale(this.baseMatrix, this.scale);
