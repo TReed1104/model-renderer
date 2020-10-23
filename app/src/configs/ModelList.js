2cfec0eeb61dc469793bf52076e45ee7c7083789
Tom Reed <t.reed1104@gmail.com>
2020-10-24 00:04:40 +0100

    Added the model configs to the ModelList module

--- app/src/configs/ModelList.js
+++ app/src/configs/ModelList.js
@@ -1,4 +1,24 @@
 // The list of models for the Engine to load
-var ModelList = {};
+var ModelList = {
+    cube: {
+        load: true,
+        enabled: true,
+        shaderIndex: 1,
+        modelFile: { path: "content/models/cube.obj"},
+        position: [0, 0, 0],
+        rotation: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    legoman: {
+        load: true,
+        enabled: false,
+        shaderIndex: 1,
+        modelFile: { path: "content/models/legoman.obj" },
+        //materialFile: {path: "content/models/legoman.mtl"},
+        position: [0, 0, 0],
+        rotation: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+};
 
 export default ModelList;
