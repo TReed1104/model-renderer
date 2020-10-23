6c55ee50fd7432a9bb7486e65a1880eaf586cf81
Tom Reed <t.reed1104@gmail.com>
2020-10-24 00:02:53 +0100

    Imported the shader code modules

--- app/src/configs/ShaderList.js
+++ app/src/configs/ShaderList.js
@@ -1,3 +1,7 @@
+// Import the Shaders
+import * as ShaderDefault from "../shaders/Default.js";
+import * as ShaderTextured from "../shaders/Textured.js";
+
 // The list of shaders for the Engine to load and compile
 var ShaderList = {}
 
