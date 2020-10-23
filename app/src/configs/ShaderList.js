1388fa20427929d39757fa177d3231494c02597d
Tom Reed <t.reed1104@gmail.com>
2020-10-24 00:03:38 +0100

    Added the shader config objects to the ShaderList module

--- app/src/configs/ShaderList.js
+++ app/src/configs/ShaderList.js
@@ -3,6 +3,17 @@ import * as ShaderDefault from "../shaders/Default.js";
 import * as ShaderTextured from "../shaders/Textured.js";
 
 // The list of shaders for the Engine to load and compile
-var ShaderList = {}
+var ShaderList = {
+    default: {
+        compile: true,
+        vertex: ShaderDefault.VertexCode,
+        fragment: ShaderDefault.FragmentCode
+    },
+    texturing: {
+        compile: true,
+        vertex: ShaderTextured.VertexCode,
+        fragment: ShaderTextured.FragmentCode
+    },
+}
 
 export default ShaderList;
