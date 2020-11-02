0f5db755432f40f7c42f389fd11639edab1864ed
Tom Reed <t.reed1104@gmail.com>
2020-11-02 01:07:15 +0000

    Idea of the light config key parsing

--- app/src/js/Light.js
+++ app/src/js/Light.js
@@ -4,5 +4,10 @@ export default class Light {
     constructor(id, config) {
         // Set the ID for the light object, unique identifier
         this.id = id;
+
+        // Parse the config object passed to the constructor
+        (config.type != undefined) ? this.type = config.type : this.type = "Invalid Type";
+        (config.position != undefined) ? this.position = config.position : this.position = [0, 0, 0];
+        (config.colour != undefined) ? this.colour = config.colour : this.colour = [1, 1, 1];
     }
 }
