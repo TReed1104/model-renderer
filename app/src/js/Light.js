9793475cb5e502d4b4b40b1665ec5056e1689169
Tom Reed <t.reed1104@gmail.com>
2020-11-02 01:06:55 +0000

    Light id/name set

--- app/src/js/Light.js
+++ app/src/js/Light.js
@@ -2,5 +2,7 @@ import { webgl } from "./Core.js";
 
 export default class Light {
     constructor(id, config) {
+        // Set the ID for the light object, unique identifier
+        this.id = id;
     }
 }
