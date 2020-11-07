0c93638d044df163cde89c442e6785ac9e40ff0d
Tom Reed <t.reed1104@gmail.com>
2020-11-07 01:59:08 +0000

    Lighting object attenuation fields

--- app/src/configs/LightList.js
+++ app/src/configs/LightList.js
@@ -1,12 +1,19 @@
 // The list of Lights for the Engine to load
 var LightList = {
     point: {
+        // Metadata
         load: true,
         type: "point",
+        // Transformations
         position: [0, 0, 0],
+        // Light colour attributes
         ambient: [0.2, 0.2, 0.2],
         diffuse: [0.5, 0.5, 0.5],
-        specular: [1, 1, 1]
+        specular: [1, 1, 1],
+        // Attenuation Values
+        constant: 1.0,
+        linear: 0.22,
+        quadratic: 0.20
     }
 };
 
