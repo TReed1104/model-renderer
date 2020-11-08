b79b20da2eef60a84456bf108aff88dd677e5728
Tom Reed <t.reed1104@gmail.com>
2020-11-08 00:37:39 +0000

    Light type value checks

--- app/src/js/Light.js
+++ app/src/js/Light.js
@@ -4,7 +4,7 @@ export default class Light {
     constructor(config) {
         // Parse the config object passed to the constructor
         (config.id != undefined) ? this.id = config.id : this.type = "Not Set";
-        (config.type != undefined) ? this.type = config.type : this.type = "Invalid Type";
+        (config.type != undefined && (config.type == "point" || config.type == "directional" || config.type == "spotlight")) ? this.type = config.type : this.type = "Invalid Type";
         (config.position != undefined) ? this.position = config.position : this.position = [0, 0, 0];
         // Lighting colour attributes
         (config.ambient != undefined) ? this.ambient = config.ambient : this.ambient = [1, 1, 1];
