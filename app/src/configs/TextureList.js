70306866887707bd188df1a936eb679b7d65223c
Tom Reed <t.reed1104@gmail.com>
2020-11-09 11:51:50 +0000

    Added TextureList module idea

---
+++ app/src/configs/TextureList.js
@@ -0,0 +1,9 @@
+// Template for a list module for assets
+var textureList = {
+    cube: {
+        load: true,
+        path: "content/textures/cubetexture.png"
+    }
+};
+
+export default textureList;
