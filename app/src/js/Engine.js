7bdfb83c53097464577b7032e6ed8e9e54bda634
Tom Reed <t.reed1104@gmail.com>
2020-11-09 11:56:49 +0000

    getIndexOfLight reverted

--- app/src/js/Engine.js
+++ app/src/js/Engine.js
@@ -179,10 +179,6 @@ export default class Engine {
     }
 
     getIndexOfLight(lightID) {
-        // Check the call is not using the default value
-        if (lightID == "Not Set") {
-            return -1;
-        }
         // Get the index of the loaded light with the specified ID
         return this.lightRegister.findIndex((light) => light.id == lightID);
     }
