1c4b00f4c775b34bec71543d1a055adf1de1bbbe
Tom Reed <t.reed1104@gmail.com>
2020-11-15 00:52:35 +0000

    Added a camera object for the main camera

--- app/src/configs/CameraList.js
+++ app/src/configs/CameraList.js
@@ -1,5 +1,13 @@
 // Template for a list module for assets
 var cameraList = {
+    main: {
+        position: [0, 2, 5],
+        lookat: [0, 0, 0],
+        up: [0, 1, 0],
+        fieldOfView: 90,
+        nearZ: 1,
+        farZ: 100
+    }
 };
 
 export default cameraList;
