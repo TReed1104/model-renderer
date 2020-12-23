5bf8c4c5a4daf3ab2ab3b8a2588456173527e80c
Tom Reed <t.reed1104@gmail.com>
2020-12-23 15:47:38 +0000

    Added id param

--- app/src/js/ShaderUniforms.js
+++ app/src/js/ShaderUniforms.js
@@ -2,7 +2,7 @@ import { webgl } from "./Core.js";
 import Shader from "./Shader.js";
 
 export default class ShaderUniforms {
-    constructor(shaderObject) {
+    constructor(id, shaderObject) {
         this.id = "Not Set"
         this.vertices = -1;
         this.colours = -1;
