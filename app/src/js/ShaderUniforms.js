9b49892005fad2db9f37c5ffbbd196d77822c99b
Tom Reed <t.reed1104@gmail.com>
2020-12-23 15:46:29 +0000

    Object fields set to default values

--- app/src/js/ShaderUniforms.js
+++ app/src/js/ShaderUniforms.js
@@ -1,4 +1,13 @@
 export default class ShaderUniforms {
     constructor(shaderObject) {
+        this.id = "Not Set"
+        this.vertices = -1;
+        this.colours = -1;
+        this.uvs = -1;
+        this.modelMatrix = -1;
+        this.viewMatrix = -1;
+        this.projectionMatrix = -1;
+        this.enableTextures = -1;
+        this.textureSampler = -1;
     }
 }
