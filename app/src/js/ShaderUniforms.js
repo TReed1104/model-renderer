eebcde8c91e4ec10f74b547295bd7003b8e77552
Tom Reed <t.reed1104@gmail.com>
2020-12-24 00:50:57 +0000

    Commenting

--- app/src/js/ShaderUniforms.js
+++ app/src/js/ShaderUniforms.js
@@ -3,13 +3,20 @@ import Shader from "./Shader.js";
 
 export default class ShaderUniforms {
     constructor(id, shaderObject) {
+        // Identifer for the Uniform mapping
         this.id = id;
+
+        // Vertex data
         this.vertexPosition = -1;
         this.vertexColour = -1;
         this.vertexUV = -1;
+
+        // MVP Matrix
         this.modelMatrix = -1;
         this.viewMatrix = -1;
         this.projectionMatrix = -1;
+
+        // Texturing
         this.enableTextures = -1;
         this.textureSampler = -1;
     }
